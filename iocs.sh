7fa89c5b31d9106a7b3a438e0f2ff03fba7235a8
vonoki <rosikonov@gmail.com>
2024-02-15 12:26:39 +0200

    qqq

---
+++ iocs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://github.com/vonoki/iocs/blob/main/ips.csv
+
+mv ips.csv /etc/graylog/
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo -e "\e[31m[!]\e[0m This script must be run with root privileges"
+  exit 1
+fi
